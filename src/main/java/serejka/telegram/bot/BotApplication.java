620ab117c71e63a8bc98dddcdc6a135331b2b2fd
Sergey Vasilchenko <sergey.vasylchenko@nixsolutions.com>
2021-01-20 00:02:01 +0200

    Add recovery database controller from heroku to admin PC.

--- src/main/java/serejka/telegram/bot/BotApplication.java
+++ src/main/java/serejka/telegram/bot/BotApplication.java
@@ -1,17 +1,20 @@
 package serejka.telegram.bot;
 
-import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
+import serejka.telegram.bot.service.CopyService;
 
-import java.sql.SQLException;
+import java.io.File;
+import java.io.IOException;
 
 
 @SpringBootApplication
 public class BotApplication {
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws IOException {
+		CopyService.copyFileUsingStream(new File("database/telegram.mv.db"),
+				new File("database/telegram-copy.mv.db"));
 		ApiContextInitializer.init();
 		SpringApplication.run(BotApplication.class, args);
 	}
---
+++ src/main/java/serejka/telegram/bot/service/CopyService.java
@@ -0,0 +1,22 @@
+package serejka.telegram.bot.service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class CopyService {
+
+    public static void copyFileUsingStream(File source, File dest) throws IOException {
+        try (InputStream is = new FileInputStream(source);
+             OutputStream os = new FileOutputStream(dest)) {
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+            }
+        }
+    }
+}
