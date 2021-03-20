d3d3f351745f0a59a9e22e01a65e0f1fed79ea23
Sergey Vasilchenko <sbkiking3@gmail.com>
2021-03-20 14:17:36 +0200

    Added Async annotations for statistics methods.

--- src/main/java/serejka/telegram/bot/logic/bot/Util.java
+++ src/main/java/serejka/telegram/bot/logic/bot/Util.java
@@ -4,6 +4,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import serejka.telegram.bot.service.StatisticsService;
@@ -18,10 +19,12 @@ public class Util {
     StatisticsService statisticsService;
     UserService userService;
 
+    @Async
     public void updateStatisticsByCommand(Message message) {
         statisticsService.updateStatisticCommand(message);
     }
 
+    @Async
     public void updateCountOfUse(Integer userId) {
         userService.updateByUserIdCountOfUse(userId);
     }
